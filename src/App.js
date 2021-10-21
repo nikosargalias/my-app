271899dc0d329582271ed90c96f78f52bec8f250
Nikos Argalias <nikolaos.papaioannou-argalias@avadolearning.com>
2021-10-21 16:03:33 +0100

    add set game duration

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import "./App.css";
 function App() {
   const TIMER = 5;
   const [text, setText] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(5);
+  const [timeRemaining, setTimeRemaining] = useState("Set Below");
   const [isGameOn, setIsGameOn] = useState(false);
   const [wordCount, setWordCount] = useState(0);
   const textareaRef = useRef(null);
+  const [gameTime, setGameTime] = useState();
 
   const handleChange = (e) => {
     const { value } = e.target;
     setText(value);
+  };
+
+  const handleSetGameTime = (e) => {
+    const { value } = e.target;
+    setGameTime(value);
   };
 
   const focusOnTextarea = () => {
@@ -20,10 +26,11 @@ function App() {
   };
 
   const startGame = () => {
-    setTimeRemaining(TIMER);
+    setTimeRemaining(gameTime || TIMER);
     setIsGameOn(true);
     setText("");
     focusOnTextarea();
+    setGameTime("");
   };
 
   const endGame = () => setIsGameOn(false);
@@ -50,12 +57,17 @@ function App() {
       <h1>How fast do you type?</h1>
       <textarea
         ref={textareaRef}
-        className="js-game-textarea"
         onChange={handleChange}
         value={text}
         disabled={!isGameOn}
       ></textarea>
       <h4>Time remaining: {timeRemaining}</h4>
+      <input
+        type="number"
+        value={gameTime}
+        placeholder="Set game duration"
+        onChange={handleSetGameTime}
+      />
       <button onClick={startGame} disabled={isGameOn}>
         Start
       </button>
