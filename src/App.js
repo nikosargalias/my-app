62506d14f6b76754c92f9fffb17670396b948db1
Nikos Argalias <nikolaos.papaioannou-argalias@avadolearning.com>
2021-10-21 13:35:13 +0100

    game working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const TIMER = 5;
+  const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(5);
+  const [isGameOn, setIsGameOn] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setText(value);
+  };
+
+  useEffect(() => {
+    const words = text.split(/\ /).filter((word) => word.length > 0);
+    setWordCount(words.length);
+  }, [text]);
+
+  useEffect(() => {
+    if (isGameOn) {
+      if (timeRemaining > 0) {
+        setTimeout(() => {
+          setTimeRemaining((time) => time - 1);
+        }, 1000);
+      } else {
+        endGame();
+      }
+    }
+  }, [timeRemaining, isGameOn]);
+
+  const focusOnTextarea = () => {
+    const textarea = document.querySelector(".js-game-textarea");
+    textarea.focus();
+  };
+
+  const startGame = () => {
+    setTimeRemaining(TIMER);
+    setIsGameOn(true);
+    setText("");
+  };
+
+  useEffect(() => {
+    if (isGameOn) {
+      focusOnTextarea();
+    }
+  }, [isGameOn]);
+
+  const endGame = () => setIsGameOn(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>How fast do you type?</h1>
+      <textarea
+        tabindex="0"
+        className="js-game-textarea"
+        onChange={handleChange}
+        value={text}
+        disabled={!isGameOn}
+      ></textarea>
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button onClick={startGame} disabled={isGameOn}>
+        Start
+      </button>
+      <h1>Word count: {wordCount}</h1>
     </div>
   );
 }
